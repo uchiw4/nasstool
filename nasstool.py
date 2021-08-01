2648e87a25d5ca947c0ce6f3a04b9fdf1954222f
Uchiw4 <77907223+uchiw4@users.noreply.github.com>
2021-08-01 20:09:08 +0200

    Update nasstool.py

--- nasstool.py
+++ nasstool.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 from pyfade import Fade, Colors
 from pycenter import center
 import discord
-import time
 import asyncio
 
 
@@ -29,37 +28,16 @@ client = discord.Client(self_bot=True)
 TOKEN = input("\033[1;31;40m Ton token :  \033")
 
 #statut
-n = int(input("\033[1;31;40m Combien de statuts veux-tu (entre 1 et 5)  ?  \033"))
-b=0
-if n == 1:
-    status1 = input("\033[1;31;40m Statut :  \033")
-    b=1
-elif n ==2:
-    status1 = input("\033[1;31;40m Statut 1:  \033")
-    status2 = input("\033[1;31;40m Statut 2:  \033")
-    b=2
-elif n ==3:
-    status1 = input("\033[1;31;40m Statut 1:  \033")
-    status2 = input("\033[1;31;40m Statut 2:  \033")
-    status3 = input("\033[1;31;40m Statut 3:  \033")
-    b=3
-elif n ==4:
-    status1 = input("\033[1;31;40m Statut 1:  \033")
-    status2 = input("\033[1;31;40m Statut 2:  \033")
-    status3 = input("\033[1;31;40m Statut 3:  \033")
-    status4 = input("\033[1;31;40m Statut 4:  \033")
-    b=4
-elif n ==5:
-    status1 = input("\033[1;31;40m Statut 1:  \033")
-    status2 = input("\033[1;31;40m Statut 2:  \033")
-    status3 = input("\033[1;31;40m Statut 3:  \033")
-    status4 = input("\033[1;31;40m Statut 4:  \033")
-    status5 = input("\033[1;31;40m Statut 5:  \033")
-    b=5
+list = []
+cmb = int(input("\033[1;31;40m Combien de statuts veux-tu ?  \033"))
+
+for i in range(cmb):
+    
+    whatstatus = input(f"\033[1;31;40m Statut {i+1}:  \033")
+    list.append(whatstatus)
 
 
-
-
+        
 
 #evenement du bot
 @client.event
@@ -67,51 +45,12 @@ async def on_ready():
     print("\033[1;31;40m Connecté au compte de {0.user} tu peux maintenant profiter de ton statut perso !\033".format(client))
     print("")
     print("\033[1;31;40m(ne ferme pas cette fenêtre si tu veux que le logiciel continue de fonctionner)\033")
-    
-
-    if b == 1:
-        await client.change_presence(activity=discord.Game(name=status1))
-    elif b == 2:
-        while True:
+    while True:
+        for i in range(cmb):
+            i = int(i)
+            status1 = list[i]
             await client.change_presence(activity=discord.Game(name=status1))
             await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status2))
-            await asyncio.sleep(10)
-    elif b == 3:
-        while True:
-            await client.change_presence(activity=discord.Game(name=status1))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status2))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status3))
-            await asyncio.sleep(10)
-    elif b == 4:
-        while True:
-            await client.change_presence(activity=discord.Game(name=status1))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status2))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status3))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status4))
-            await asyncio.sleep(10)
-    elif b == 5:
-        while True:
-            await client.change_presence(activity=discord.Game(name=status1))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status2))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status3))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status4))
-            await asyncio.sleep(10)
-            await client.change_presence(activity=discord.Game(name=status5))
-            await asyncio.sleep(10)
-
-
-        
-
-
 
 #connection
 
