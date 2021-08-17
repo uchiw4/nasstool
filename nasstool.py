7b5f143de1447f132c2d57891996b4a5fefb4dbe
Uchiw4 <77907223+uchiw4@users.noreply.github.com>
2021-08-17 15:58:19 +0200

    nasstool2.0
    Tu peux maintenant enregistrer des profils et les charger directement en lançant le logiciel !

--- nasstool.py
+++ nasstool.py
@@ -3,7 +3,7 @@ from pyfade import Fade, Colors
 from pycenter import center
 import discord
 import asyncio
-
+import json
 
 text ="""
 ███╗   ██╗ █████╗ ███████╗███████╗████████╗ ██████╗  ██████╗ ██╗     
@@ -21,23 +21,69 @@ print(Fade.Vertical(Colors.red_to_blue, center(text)))
 
 
 
-
 client = discord.Client(self_bot=True)
 
-#entrée du token
-TOKEN = input("\033[1;31;40m Ton token :  \033")
 
-#statut
-list = []
-cmb = int(input("\033[1;31;40m Combien de statuts veux-tu ?  \033"))
+with open("hello.json","r+") as f:
+        data_raw = json.load(f)
+        stattry = data_raw["token"]
 
-for i in range(cmb):
-    
-    whatstatus = input(f"\033[1;31;40m Statut {i+1}:  \033")
-    list.append(whatstatus)
+if stattry == "":
+    pass
+else:
+    loadstatus = input("\033[1;31;40m Tu as déjà un profil enregistré, souhaites-tu le charger ? [o/n] \033")
+    if loadstatus =="o":
+        with open("hello.json","r+") as f:
+            data_raw = json.load(f)
+            TOKEN = data_raw["token"]
+            statuslist = data_raw["statuts"]
+            cmb = data_raw["cmb"]
+        print("\033[1;31;40m Profil chargé avec succès !  \033")
+
+    else:
+        pass
 
 
+
+if stattry == "":
+    TOKEN = input("\033[1;31;40m Ton token :  \033")
+
+    #statut
+    statuslist = []
+    cmb = int(input("\033[1;31;40m Combien de statuts veux-tu ?  \033"))
+
+    for i in range(cmb):
         
+        whatstatus = input(f"\033[1;31;40m Statut {i+1}:  \033")
+        statuslist.append(whatstatus)
+    
+    if stattry == "":
+
+        asktosave = input("\033[1;31;40m Souhaites-tu enregistrer un nouveau profil pour pouvoir le réutiliser la prochaine fois ? [o/n]  \033")
+        if asktosave == "o":
+            with open("hello.json","r+") as f:
+                data_raw = json.load(f)
+                data_raw["token"] = TOKEN
+                data_raw["statuts"] = statuslist
+                data_raw["cmb"] = cmb
+                f.seek(0)
+                json.dump(data_raw,f,indent=4)
+                f.truncate() 
+    
+    else:
+        alreadysaved = input("\033[1;31;40m Je vois que tu as déjà un profil enregistré souhaites-tu le remplacer par ce que tu viens de créer ?  [o/n]  \033")
+        if alreadysaved == "o":
+            with open("hello.json","r+") as f:
+                data_raw = json.load(f)
+                data_raw["token"] = TOKEN
+                data_raw["statuts"] = statuslist
+                data_raw["cmb"] = cmb
+
+                f.seek(0)
+                json.dump(data_raw,f,indent=4)
+                f.truncate() 
+
+
 
 #evenement du bot
 @client.event
@@ -47,15 +93,10 @@ async def on_ready():
     print("\033[1;31;40m(ne ferme pas cette fenêtre si tu veux que le logiciel continue de fonctionner)\033")
     while True:
         for i in range(cmb):
-            i = int(i)
-            status1 = list[i]
+            status1 = statuslist[i]
             await client.change_presence(activity=discord.Game(name=status1))
             await asyncio.sleep(10)
 
 #connection
 
 client.run(TOKEN, bot=False)
-    
-
-
-
